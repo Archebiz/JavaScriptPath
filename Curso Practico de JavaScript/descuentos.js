15481589b0d1387e1f653717d0ed13b8b6b6164a
Archebiz <adriansalazar2497@gmail.com>
2022-06-09 22:06:43 -0500

    Reto Cupones

--- Curso Practico de JavaScript/descuentos.js
+++ Curso Practico de JavaScript/descuentos.js
@@ -1,24 +1,20 @@
-// const precioOriginal= 120;
-// const descuento =18;
-
-// const porcentajePrecioConDescuento=100-descuento;
-
-// const precioConDescuento=porcentajePrecioConDescuento*precioOriginal/100;
-
-
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento,
-// })
+var cupones=["CUPON10","CUPON20"]
 
 function calcularPrecioConDescuento(precio,descuento){
     const porcentajePrecioConDescuento=100-descuento;
     const precioConDescuento=(precio*porcentajePrecioConDescuento)/100;
     return precioConDescuento;
 }
-
+function descuentoCuponera(precio,cupon){
+    switch(cupon){
+        case cupones[0]:
+            return calcularPrecioConDescuento(precio,10);            
+        case cupones[1]:
+            return calcularPrecioConDescuento(precio,20);
+        default:
+            return "Cupon no Existe"
+    }    
+}
 
 function onClickButtonPriceDiscount(){
     const inputPrice= document.getElementById("InputPrice");
@@ -28,5 +24,16 @@ function onClickButtonPriceDiscount(){
     const precioConDescuento=calcularPrecioConDescuento(priceValue,DiscountValue)
     const resultP=document.getElementById("ResultP");
     resultP.innerText="El precio con descuento son:"+precioConDescuento+ "$"
+
+}
+
+function onClickButtonCuponDiscount(){
+    const inputPrice= document.getElementById("InputPrice");
+    const priceValue=inputPrice.value;
+    const inputCupon= document.getElementById("InputCupon");
+    const Cupon =inputCupon.value;
+    const precioConDescuento=descuentoCuponera(priceValue,Cupon)
+    const ResultPbyCupon=document.getElementById("ResultPbyCupon");
+    ResultPbyCupon.innerText="El precio con descuento son:"+precioConDescuento+ "$"
 
 }
